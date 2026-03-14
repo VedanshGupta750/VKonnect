eccc499a1ba349efead46f9927e3c49062e39049
VedanshGupta2533 <guptavp@rknec.edu>
2026-03-14 23:42:36 +0530

    /auth page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage.jsx";
+import Authentication from "./pages/Authentication.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<Authentication/>} />
       </Routes>
     </Router>
   );
