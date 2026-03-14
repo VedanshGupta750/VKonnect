a14cd35cc225a9610419f1fb1c58a2caba7544e1
VedanshGupta2533 <guptavp@rknec.edu>
2026-03-14 22:13:48 +0530

    Landing Page Done

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
-
+import '../App.css'
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
+            <div className='landingPageContainer'>
+        <nav>
+            <div className='navHeader'>
+                <h2>VKonnect</h2>
+            </div>
+            <div className='navList'>
+                <p>Join as Guest</p>
+                <p>Register</p>
+                <div role='button'>Login</div>
+            </div>
+        </nav>
+
+        <div className="landingMainContainer">
+        <div id='connect'>
+            <h1><span style={{color:'#FF9839'}}>Connect</span> with your Loved Ones</h1>
+            <p>Cover Distance by VKonnect</p>
+            <div id="get-started">
+                <a href='/home'>Get started</a>
+            </div>
+        </div>
+        <div className='mobile-image'>
+            <img src='/mobile.png'/>
+
+        </div>
+    </div>
+    </div>
+
+
+
   )
 }
