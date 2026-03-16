c0ec4228b2dc0bf1a97b48baecb990337d7f05bb
VedanshGupta2533 <guptavp@rknec.edu>
2026-03-16 18:08:34 +0530

    videomeet component joint in APP.jsx and declaration of Statev= Variable in VideoMeet component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.jsx";
 import Authentication from "./pages/Authentication.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import VideoMeet from "./pages/VideoMeet.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <AuthProvider><Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<Authentication/>} />
+        <Route path='/:url' element={<VideoMeet/>}/>
       </Routes></AuthProvider>
     </Router>
   );
