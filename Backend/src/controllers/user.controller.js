8006ca707e993e35a6d9cf83c1967fc348c5d7a8
VedanshGupta2533 <guptavp@rknec.edu>
2026-04-03 17:18:20 +0530

    get history and add to history in backend

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js";
 import httpStatus from "http-status";
 import bcrypt from "bcrypt"
 import crypto from "crypto";
+import { Meeting } from "../models/meeting.model.js";
 
 //register path
 const register = async(req , res) =>{
@@ -69,4 +70,37 @@ const login = async (req, res) => {
   }
 };  
 
-export {login ,register}
+const getUserHistory =async (req ,res)=>{
+  const {token} = res.query;
+  try{
+    const user = await User.findOne({
+      token: token
+    })
+    const meetings = await Meeting.findOne({
+      user_id: user.username
+    })
+    res.json(meetings);
+  }catch(e){
+    res.json({message: `Something went wrong ${e}`})
+  }
+}
+
+const addToHistory = async (req , res) =>{
+  const {token , meeting_code} = req.body;
+  try{
+    const user = await User.findOne({
+      token: token
+    })
+    const new_Meeting = new Meeting({
+      user_id: user.username,
+      meetingCode: meeting_code
+    })
+    await new_Meeting.save();
+    res.status(httpStatus.CREATED).json({message: "Added code to history"});
+  }
+  catch(e){
+    res.json({message: `Something went wrong ${e}`})
+  }
+}
+
+export {login ,register , getUserHistory , addToHistory}
